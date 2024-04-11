fecde75fbdfe5eab2b708295a8b88cbff8cf42c4
pythonian23 <pythonian23@gmail.com>
2024-04-12 01:09:50 +0900

    did neoliberal policies on variables

--- main.c
+++ main.c
@@ -7,14 +7,14 @@
 #define LOG_LEVEL LOG_DEBUG
 #endif
 
-static int tps;
-static Camera2D camera;
-static float scale;
+static int _tps;
+static Camera2D _camera;
+static float _scale;
 
-static bool ui_focused = false;
-static Rectangle picker_bounds = { 0, 0, 256, 256 };
+static bool _ui_focused = false;
+static Rectangle _picker_bounds = { 0, 0, 256, 256 };
 
-static Color main_colour = { 255, 0, 255, 255 };
+static Color _main_colour = { 255, 0, 255, 255 };
 
 static void init_raylib(void)
 {
@@ -23,7 +23,7 @@ static void init_raylib(void)
 	InitWindow(1280, 720, "rawr Pixel Art Editor");
 	SetExitKey(KEY_Q);
 
-	camera = (Camera2D) {
+	_camera = (Camera2D) {
 	0};
 }
 
@@ -32,51 +32,51 @@ static void init(void)
 	init_raylib();
 	init_canvas();
 
-	scale = 1;
+	_scale = 1;
 }
 
-static bool mouse_in_ui()
+static bool _mouse_in_ui()
 {
-	return (CheckCollisionPointRec(GetMousePosition(), picker_bounds));
+	return (CheckCollisionPointRec(GetMousePosition(), _picker_bounds));
 }
 
-static Vector2 mouse_position(void)
+static Vector2 _mouse_position(void)
 {
-	return GetScreenToWorld2D(GetMousePosition(), camera);
+	return GetScreenToWorld2D(GetMousePosition(), _camera);
 }
 
-static void handle_input(void)
+static void _handle_input(void)
 {
 
 	static Vector2 draw_position;
 	static Vector2 draw_position_prev;
 
 	if (IsKeyPressed(KEY_MINUS))
-		scale /= 1.2;
+		_scale /= 1.2;
 	if (IsKeyPressed(KEY_EQUAL))
-		scale *= 1.2;
+		_scale *= 1.2;
 	if (IsKeyPressed(KEY_ZERO))
-		scale = 1;
+		_scale = 1;
 
 	if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-		if (mouse_in_ui())
-			ui_focused = true;
+		if (_mouse_in_ui())
+			_ui_focused = true;
 		else {
-			draw_position = mouse_position();
+			draw_position = _mouse_position();
 			draw_position_prev = draw_position;
 		}
 	}
-	if (IsMouseButtonDown(MOUSE_BUTTON_LEFT) && !ui_focused) {
-		draw_position = mouse_position();
-		cvs_draw_line(draw_position_prev, draw_position, main_colour);
+	if (IsMouseButtonDown(MOUSE_BUTTON_LEFT) && !_ui_focused) {
+		draw_position = _mouse_position();
+		cvs_draw_line(draw_position_prev, draw_position, _main_colour);
 		draw_position_prev = draw_position;
 	}
 	if (IsMouseButtonUp(MOUSE_BUTTON_LEFT)) {
-		ui_focused = false;
+		_ui_focused = false;
 	}
 
 	if (IsMouseButtonPressed(MOUSE_BUTTON_MIDDLE)) {
-		cvs_fill(mouse_position(), main_colour);
+		cvs_fill(_mouse_position(), _main_colour);
 	}
 
 	if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT))
@@ -92,31 +92,31 @@ static void handle_input(void)
 			TraceLog(LOG_WARNING, "Image save failed!");
 }
 
-static void handle_camera(void)
+static void _handle_camera(void)
 {
-	camera.target = (Vector2) {
+	_camera.target = (Vector2) {
 	cvs_get_width() * 0.5, cvs_get_height() * 0.5};
-	camera.offset = (Vector2) {
+	_camera.offset = (Vector2) {
 	GetScreenWidth() * 0.5, GetScreenHeight() * 0.5};
-	camera.zoom =
+	_camera.zoom =
 	    (GetScreenWidth() < GetScreenHeight()?
-	     GetScreenWidth() * scale / cvs_get_width() :
-	     GetScreenHeight() * scale / cvs_get_height());
+	     GetScreenWidth() * _scale / cvs_get_width() :
+	     GetScreenHeight() * _scale / cvs_get_height());
 }
 
-static void render(void)
+static void _render(void)
 {
 	BeginDrawing();
 	{
 		ClearBackground(BLACK);
 
-		BeginMode2D(camera);
+		BeginMode2D(_camera);
 		{
 			DrawTexture(cvs_get_texture(), 0, 0, WHITE);
 		}
 		EndMode2D();
 
-		GuiColorPanel(picker_bounds, "Colour Picker", &main_colour);
+		GuiColorPanel(_picker_bounds, "Colour Picker", &_main_colour);
 
 		DrawFPS(0, 0);
 	}
@@ -134,9 +134,9 @@ int main(void)
 	}
 
 	while (!WindowShouldClose()) {
-		handle_input();
-		handle_camera();
-		render();
+		_handle_input();
+		_handle_camera();
+		_render();
 	}
 
 	CloseWindow();
